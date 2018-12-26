1099c95941847018129b38e57b3ead759543e278
Abhilash <abhilash.korraprolu@gmail.com>
2018-12-26 01:00:56 +0100

    Difficulty: 3.5

---
+++ A Prize No One Can Win/README.md
@@ -0,0 +1,1 @@
+![description](https://user-images.githubusercontent.com/42868745/50427452-f6397780-08a8-11e9-92ea-3d6a4954cf8f.jpg)
---
+++ A Prize No One Can Win/Rig.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Rig
+{
+    public static void main(String args[]) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int noOfItems, X;
+        int[] itemPrices;
+
+        // Input: Line 1
+        String[] ip1 = br.readLine().split(" ");
+        noOfItems =  Integer.parseInt(ip1[0]);
+        X = Integer.parseInt(ip1[1]);
+
+        if(noOfItems == 1)
+        {
+            System.out.println(1);
+            System.exit(0);
+        }
+
+        // Input: Line 2
+        String[] ip2 = br.readLine().split(" ");
+
+        itemPrices = new int[noOfItems];
+
+        for (int i = 0; i < noOfItems; i++)
+        {
+            itemPrices[i] = Integer.parseInt(ip2[i]);
+        }
+
+        Arrays.sort(itemPrices);
+
+        for(int i = 1; i < noOfItems; i++)
+        {
+            int sum = itemPrices[i-1] + itemPrices[i];
+
+            if(sum > X)
+            {
+                System.out.println(i);
+                System.exit(0);
+            }
+        }
+        System.out.println(noOfItems);
+    }
+}
