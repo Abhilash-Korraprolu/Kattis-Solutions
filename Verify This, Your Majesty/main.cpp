f3d142461ac82581473a0af19fcfe386c0c67772
Abhilash047 <abhilash.korraprolu@gmail.com>
2019-07-09 13:18:13 +0200

    Difficulty: 4.1

---
+++ Verify This, Your Majesty/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n;
+    cin >> n;
+    int coordinates[n * 2];
+    
+    for (int i = 0; i < n * 2; i++) {
+        int x, y;
+        cin >> x >> y;
+        coordinates[i] = x;
+        coordinates[++i] = y;
+        
+        for (int j = i - 2; j >= 0; j--) {
+            int a = abs(coordinates[i] - coordinates[j]);
+            int b = abs(coordinates[i - 1] - coordinates[--j]);
+            
+            if (a == b || a == 0 || b == 0) {
+                cout << "INCORRECT";
+                return 0;
+            }
+        }
+    }
+    
+    cout << "CORRECT";
+    return 0;
+}
